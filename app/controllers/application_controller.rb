ff5674227fb6b9c0a15e3bcb01cb325335f98377
parasbansal-87 <parasbansal33@gmail.com>
2018-03-21 17:45:23 -0500

    Edit Action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "Dawn of a new Era"
+  	render html: "Ronnie O Sullivan beats Ding Junhui 6-3 to reach the Semis"
   end
 end
